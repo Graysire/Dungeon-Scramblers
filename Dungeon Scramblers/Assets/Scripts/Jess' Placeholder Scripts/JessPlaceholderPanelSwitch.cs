87a27eea751053b70942c2144912cc9c836f27ab
Jessica Wei <jessicawei9@gmail.com>
2020-09-22 15:05:35 -0700

    demo ready

---
+++ Dungeon Scramblers/Assets/Scripts/Jess' Placeholder Scripts/JessPlaceholderPanelSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class JessPlaceholderPanelSwitch : MonoBehaviour
+{
+    public GameObject TitleScreen;
+    public GameObject LoadoutScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToTitleScreen() {
+        LoadoutScreen.SetActive(false);
+        TitleScreen.SetActive(true);
+    }    
+    public void GoToLoadoutScreen() {
+        TitleScreen.SetActive(false);
+        LoadoutScreen.SetActive(true);
+    }
+}
