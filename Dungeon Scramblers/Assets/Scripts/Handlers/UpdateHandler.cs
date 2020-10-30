6fc06bd6afd14dbea9df7e9244c6831fea49eef0
Jessica Wei <jessicawei9@gmail.com>
2020-10-29 17:56:42 -0700

    Player script init, UpdateHandler script is set up

---
+++ Dungeon Scramblers/Assets/Scripts/Handlers/UpdateHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateHandler : MonoBehaviour
+{
+    public delegate void onUpdate();
+    public static event onUpdate UpdateOccurred; // Use this variable to add events/methods onto
+                                                 /* Usage (must have both enable and disable):
+                                                  * -----------------------------------------------
+                                                  * private void OnEnable(){
+                                                  *     UpdateHandler.UpdateOccurred += methodName;
+                                                  * }
+                                                  * private void OnDisable(){
+                                                  *     UpdateHandler.UpdateOccurred -= methodName;
+                                                  * }
+                                                  */
+
+    private void Update()
+    {
+        if (UpdateOccurred != null)             // If there are methods attached to the UpdateOccurred event...
+            UpdateOccurred();                   // Call all of those methods at the same time in one Update() function
+    }
+}
