7e2c0320b93e9a37dbfbc736e1abd9c00fa4d25e
Jessica Wei <jessicawei9@gmail.com>
2020-11-03 16:55:32 -0800

    - Updated UpdateHandler to also include OnFixedUpdate for physics-based operations - Completed shell of Player class - Installed InputSystem for easier cross-platform inputs

--- Dungeon Scramblers/Assets/Scripts/Handlers/UpdateHandler.cs
+++ Dungeon Scramblers/Assets/Scripts/Handlers/UpdateHandler.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class UpdateHandler : MonoBehaviour
 {
     public delegate void onUpdate();
-    public static event onUpdate UpdateOccurred; // Use this variable to add events/methods onto
-                                                 /* Usage (must have both enable and disable):
-                                                  * -----------------------------------------------
-                                                  * private void OnEnable(){
-                                                  *     UpdateHandler.UpdateOccurred += methodName;
-                                                  * }
-                                                  * private void OnDisable(){
-                                                  *     UpdateHandler.UpdateOccurred -= methodName;
-                                                  * }
-                                                  */
+    public static event onUpdate UpdateOccurred;     // Use this variable to add events/methods onto
+                                                     /* Usage (must have both enable and disable):
+                                                      * -----------------------------------------------
+                                                      * private void OnEnable(){
+                                                      *     UpdateHandler.UpdateOccurred += methodName;
+                                                      * }
+                                                      * private void OnDisable(){
+                                                      *     UpdateHandler.UpdateOccurred -= methodName;
+                                                      * }
+                                                      */
+
+    public static event onUpdate FixedUpdateOccurred; // Physics-based updates
 
     private void Update()
     {
-        if (UpdateOccurred != null)             // If there are methods attached to the UpdateOccurred event...
-            UpdateOccurred();                   // Call all of those methods at the same time in one Update() function
+        if (UpdateOccurred != null)                   // If there are methods attached to the UpdateOccurred event...
+            UpdateOccurred();                         // Call all of those methods at the same time in one Update() function
+    }
+
+    private void FixedUpdate()
+    {
+        if (FixedUpdateOccurred != null)
+            FixedUpdateOccurred();
     }
 }
--- Dungeon Scramblers/Assets/Scripts/Player Architecture/Player.cs
+++ Dungeon Scramblers/Assets/Scripts/Player Architecture/Player.cs
@@ -4,24 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] protected Rigidbody2D rb;
     protected enum Stats { 
         health = 0,
-        speed = 1,
+        movespeed = 1,
+        attackdmg = 2,
+        attackspeed = 3,
+        abilitydmg = 4,
+        defense = 5
     }
 
-    //protected 
+    [SerializeField] protected float[] stats = new float[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+    protected bool isDead = false;
 
     private void OnEnable()
     {
         UpdateHandler.UpdateOccurred += testerMethod;
+        UpdateHandler.FixedUpdateOccurred += Move;
     }    
     private void OnDisable()
     {
         UpdateHandler.UpdateOccurred -= testerMethod;
+        UpdateHandler.FixedUpdateOccurred -= Move;
     }
 
     private void testerMethod() {
-        Debug.Log("Health = " + Stats.health);
-        Debug.Log("Speed = " + Stats.speed.ToString());
+        Debug.Log("Health = " + (int)Stats.health);
+        Debug.Log("Speed = " + Stats.movespeed.ToString());
+    }
+
+    protected virtual void Move(){
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb.AddForce(movement * stats[(int)Stats.movespeed]);
+    }
+
+    protected virtual void Attack() { 
+        // BUTTONS TO ATTACK
+    }
+
+    protected virtual void UseAbility() { 
+        // BUTTONS TO USE ABILITY
+    }
+
+    protected virtual void Die() {
+        if (stats[(int)Stats.health] <= 0) {
+            Debug.Log("Do something that indicates the player is dead...");
+            isDead = true;
+            // Death animation
+        }
+    }
+    protected virtual void Vote() { 
+        // was this supposed to be a thing?
     }
 }
