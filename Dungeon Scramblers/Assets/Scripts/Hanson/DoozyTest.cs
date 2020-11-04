ae535a1b2a8e0f83015919a63af4e5d207954875
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-11-03 20:22:37 -0800

    Tested Out doozy with working scripts and buttons, Voting System WIP

---
+++ Dungeon Scramblers/Assets/Scripts/Hanson/DoozyTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoozyTest : MonoBehaviour
+{
+    [SerializeField]
+    private Text Count;
+
+    int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncrementCount()
+    {
+        count++;
+        Count.text = "Count: " + count.ToString();
+    }
+
+    public void DecrementCount()
+    {
+        count--;
+        Count.text = "Count: " + count.ToString();
+    }
+}
