893ac52cd17f219dcd9e1e54cf933ae5d434be86
Graysire <graysire@dyrix.net>
2020-11-05 11:27:11 -0800

    Added recursive room generation, it will likely needto be removed or refactored, but it creates fun looking results

--- Dungeon Scramblers/Assets/Scripts/Map Generation/MapMaker.cs
+++ Dungeon Scramblers/Assets/Scripts/Map Generation/MapMaker.cs
@@ -32,10 +32,10 @@ public class MapMaker : MonoBehaviour
     {
         
         tilemap.SetTile(new Vector3Int(0, 0, 0), doorTile);
-        //GenerateRoom(new Vector3Int(0, 0, 0), 0);
+        GenerateRoom(new Vector3Int(0, 0, 0), 0);
         //GenerateRoom(new Vector3Int(0, 0, 0), 1);
         //GenerateRoom(new Vector3Int(0, 0, 0), 2);
-        GenerateRoom(new Vector3Int(0, 0, 0), 3);
+        //GenerateRoom(new Vector3Int(0, 0, 0), 3);
     }
 
     // Update is called once per frame
@@ -51,6 +51,8 @@ public class MapMaker : MonoBehaviour
     //Exception to the above is generating 0 and, 1 and 3, 0 then 3 then 2, or 0 then 1 then 2
     void GenerateRoom(Vector3Int doorPosition, int doorDirection)
     {
+        int newRoomChance = 35;
+
         //generate a random x size of the floor space of the room, multiply by negative one if the direction is left
         int randX = Random.Range(minRoomSize, maxRoomSize);// * (doorDirection == 3 ? -1 : 1);
         //generate a random x size of the floor space of the room, multiply by negative one if the direction is down
@@ -101,32 +103,114 @@ public class MapMaker : MonoBehaviour
         //fill the inside of the room with floor tiles
         tilemap.BoxFill(new Vector3Int(startX, startY, 0), floorTile,startX,startY,startX+randX - 1,startY+randY - 1);
 
-        
-
+        Vector3Int randDoorLocation;
+        if (doorDirection != 0 && Random.Range(0,100) < newRoomChance)
+        {
+            randDoorLocation = new Vector3Int(startX + Random.Range(1, randX - 1), startY - 1, 0);
+            tilemap.SetTile(randDoorLocation, doorTile);
+            GenerateRoom(randDoorLocation, 2);
+        }
+        if (doorDirection != 1 && Random.Range(0, 100) < newRoomChance)
+        {
+            randDoorLocation = new Vector3Int(startX - 1, startY + Random.Range(1, randY - 1), 0);
+            tilemap.SetTile(randDoorLocation, doorTile);
+            GenerateRoom(randDoorLocation, 3);
+        }
+        if (doorDirection != 2 && Random.Range(0, 100) < newRoomChance)
+        {
+            randDoorLocation = new Vector3Int(startX + Random.Range(1, randX - 1), startY + randY, 0);
+            tilemap.SetTile(randDoorLocation, doorTile);
+            GenerateRoom(randDoorLocation, 0);
+        }
+        if (doorDirection != 3 && Random.Range(0, 100) < newRoomChance)
+        {
+            randDoorLocation = new Vector3Int(startX + randX, startY + Random.Range(1, randY - 1), 0);
+            tilemap.SetTile(randDoorLocation, doorTile);
+            GenerateRoom(randDoorLocation, 1);
+        }
+        /*
         //based on door direction, place doors
         switch (doorDirection)
         {
             case 0:
-                tilemap.SetTile(new Vector3Int(startX - 1, startY + Random.Range(1, randY - 1), 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + randX, startY + Random.Range(1, randY - 1), 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + Random.Range(1,randX-1), startY + randY, 0), doorTile);
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX - 1, startY + Random.Range(1, randY - 1), 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 3);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + randX, startY + Random.Range(1, randY - 1),0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 1);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + Random.Range(1, randX - 1), startY + randY, 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 0);
+                }
                 break;
             case 1:
-                tilemap.SetTile(new Vector3Int(startX + randX, startY + Random.Range(1, randY - 1), 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + Random.Range(1, randX - 1), startY + randY, 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + Random.Range(1, randX - 1), startY - 1, 0), doorTile);
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + randX, startY + Random.Range(1, randY - 1), 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 1);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + Random.Range(1, randX - 1), startY + randY, 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 0);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    
+                }
                 break;
             case 2:
-                tilemap.SetTile(new Vector3Int(startX - 1, startY + Random.Range(1, randY - 1), 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + randX, startY + Random.Range(1, randY - 1), 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + Random.Range(1, randX - 1), startY - 1, 0), doorTile);
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX - 1, startY + Random.Range(1, randY - 1), 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 3);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + randX, startY + Random.Range(1, randY - 1), 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 1);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + Random.Range(1, randX - 1), startY - 1, 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 2);
+                }
                 break;
             case 3:
-                tilemap.SetTile(new Vector3Int(startX - 1, startY + Random.Range(1, randY - 1), 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + Random.Range(1, randX - 1), startY + randY, 0), doorTile);
-                tilemap.SetTile(new Vector3Int(startX + Random.Range(1, randX - 1), startY - 1, 0), doorTile);
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX - 1, startY + Random.Range(1, randY - 1), 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 3);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + Random.Range(1, randX - 1), startY + randY, 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 0);
+                }
+                if (Random.Range(0, newRoomChance) < 1)
+                {
+                    Vector3Int randDoorLocation = new Vector3Int(startX + Random.Range(1, randX - 1), startY - 1, 0);
+                    tilemap.SetTile(randDoorLocation, doorTile);
+                    GenerateRoom(randDoorLocation, 2);
+                }
                 break;
-        }
+        }*/
     }
 
 
