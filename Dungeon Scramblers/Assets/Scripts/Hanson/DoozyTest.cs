f9f245dce9246eb36ddb0f8d4f6447c32c92691c
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-11-05 19:56:30 -0800

    working UI for voting on perk, need to add timer and popup event for it

--- Dungeon Scramblers/Assets/Scripts/Hanson/DoozyTest.cs
+++ Dungeon Scramblers/Assets/Scripts/Hanson/DoozyTest.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Doozy.Engine.UI;
 
 public class DoozyTest : MonoBehaviour
 {
     [SerializeField]
-    private Text Count;
-
-    int count = 0;
+    private UIButton[] Buttons;
+    int[] count = {0,0,0};
     // Start is called before the first frame update
     void Start()
     {
@@ -21,15 +21,15 @@ public class DoozyTest : MonoBehaviour
         
     }
 
-    public void IncrementCount()
+    public void IncrementCount(int index)
     {
-        count++;
-        Count.text = "Count: " + count.ToString();
+        count[index]++;
+        Buttons[index].SetLabelText(count[index].ToString());
     }
 
-    public void DecrementCount()
+    public void DecrementCount(int index)
     {
-        count--;
-        Count.text = "Count: " + count.ToString();
+        count[index]--;
+        Buttons[index].SetLabelText(count[index].ToString());
     }
 }
