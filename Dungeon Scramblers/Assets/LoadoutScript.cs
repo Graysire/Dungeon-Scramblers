26c6cb111771508c131891693bac780e9f70e4f1
Chloe <c.culver831@gmail.com>
2020-11-05 17:54:59 -0800

    menu proto

---
+++ Dungeon Scramblers/Assets/LoadoutScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadoutScript : MonoBehaviour
+{
+    public GameObject ArmorPanel;
+    public GameObject WeaponsPanel;
+    public GameObject OtherShitPanel;
+
+    public void ShowArmor()
+    {
+        ArmorPanel.SetActive(true);
+        WeaponsPanel.SetActive(false);
+        OtherShitPanel.SetActive(false);
+    }
+    public void ShowWeapons()
+    {
+        ArmorPanel.SetActive(false);
+        WeaponsPanel.SetActive(true);
+        OtherShitPanel.SetActive(false);
+    }
+
+    public void ShowOtherShit()
+    {
+        ArmorPanel.SetActive(false);
+        WeaponsPanel.SetActive(false);
+        OtherShitPanel.SetActive(true);
+    }
+}
