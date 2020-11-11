70b1d03a56316c11dab2b111980ca3f8e9e45533
Chloe <c.culver831@gmail.com>
2020-11-10 19:17:30 -0800

    connecting players to photon services via name: working

---
+++ Dungeon Scramblers/Assets/Scripts/Chloe Scripts/NetworkManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [Header("Login Ui Panel")]
+    public InputField playerNameInput;
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+
+    #region UI Callbacks
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = playerNameInput.text;
+        if(!string.IsNullOrEmpty(playerName))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+
+
+        }
+        else
+        {
+            Debug.Log("Invalid Name: " + playerName + " please try again");
+        }
+    }
+
+    #endregion
+
+
+
+    #region Photon Callbacks
+
+    public override void OnConnected()
+    {
+        Debug.Log("Player has connected to internet");
+        base.OnConnected();
+    }
+    public override void OnConnectedToMaster()
+    {
+
+        Debug.Log("Player: " + PhotonNetwork.LocalPlayer.NickName + " has been connected to Photon Services");
+        base.OnConnectedToMaster();
+    }
+
+
+
+    #endregion
+}
