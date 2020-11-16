179bf0cabdf512ba3e973ad91c10b750dada5a18
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-11-15 19:16:04 -0800

    Overlord Selecting Obstacles and Place Obstacles WIP

---
+++ Dungeon Scramblers/Assets/Scripts/Hanson's/Scripts/MapNode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNode : MonoBehaviour
+{
+    [SerializeField]
+    protected Color hoverColor;
+    
+    private SpriteRenderer render;
+    private Color startColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        startColor = render.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Hovered");
+        render.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        render.material.color = startColor;
+    }
+}
