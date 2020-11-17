27f3e61f5063f3e24ded0333e26f0ff7b1744835
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-11-17 02:30:51 -0800

    Placement of obstacles and enemy WIP

---
+++ Dungeon Scramblers/Assets/OverlordUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverlordUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ObstacleClicked(GameObject EnemyInstance)
+    {
+        Image image = EnemyInstance.GetComponent<Image>();
+        Hover.GetHover().Activate(image.sprite);
+        Hover.GetHover().SetEnemyIntance(EnemyInstance);
+    }
+
+   
+}
---
+++ Dungeon Scramblers/Assets/Scripts/Hanson's/Scripts/Hover.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    // Making this class a singleton will allow all obstacles to call on the activate function and replace their
+    // sprite into the hover instance while the mopuse is down
+    private static Hover HoverInstance;
+
+    private GameObject EnemyInstance;
+    private SpriteRenderer obstacle;
+
+    public static Hover GetHover()
+    {
+        
+        return HoverInstance;
+    }
+
+    private void Awake()
+    {
+        HoverInstance = this;
+    }
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstacle = this.GetComponent<SpriteRenderer>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMouse();
+    }
+
+    private void FollowMouse()
+    {
+        if(obstacle.enabled)
+        {
+            Vector3 MouseWorldCoord = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(MouseWorldCoord.x, MouseWorldCoord.y, 0);
+        }
+    }
+
+    public void Activate(Sprite sprite)
+    {
+        obstacle.sprite = sprite;
+        obstacle.enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        obstacle.enabled = false;
+    }
+
+    public void SetEnemyIntance(GameObject EnemyInstance)
+    {
+        this.EnemyInstance = EnemyInstance;
+    }
+
+    public void CreateEnemyInstacne(Transform trans)
+    {
+        if(EnemyInstance != null)
+        {
+            GameObject.Instantiate(EnemyInstance, trans, true);
+        }
+        
+    }
+
+
+    //private void OnMouseUp()
+    //{
+    //    Debug.Log("Mouse Up");
+    //    Deactivate();
+    //}
+}
