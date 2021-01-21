f1d7ca34b3359fdf381732514e7c4d1dfa3aba44
nguyenhanson1 <nguyenhanson1@gmail.com>
2021-01-21 14:13:36 -0800

    transfer attack script on its own and require player to add a component for it to work.

--- Dungeon Scramblers/Assets/Scripts/Hanson's/Scripts/Attack.cs
+++ Dungeon Scramblers/Assets/Scripts/Hanson's/Scripts/Attack.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Ability Ability;
+    private HansonsOverlord Player;
+    private Vector3 AttackDirection;
+
+
+    public void StartAttack(Vector3 AttackDirection, Ability Ability, HansonsOverlord Player)
     {
-        
+        this.AttackDirection = AttackDirection;
+        this.Ability = Ability;
+        this.Player = Player;
+        StartCoroutine("Attacking");
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator Attacking()
     {
-        
+        // Set Is currently attacking
+        Player.toggleIsAttacking();
+        // Wait for ability casting time before proceeding
+        yield return new WaitForSeconds(Ability.GetCastingTime());
+        // get mouse coordinate from camera when clicked and find the ending of the attack with the mouse clicked
+
+        Vector3 AttackEnd = Player.transform.position + AttackDirection;
+        // Get relative position of where the mouse was clicked to correctly calculate the angle for projectile
+        Vector3 RelativeAttackEnd = AttackEnd - Player.transform.position;
+        float dot = Vector3.Dot(Player.transform.up, RelativeAttackEnd);
+
+        // Calculate the angle of the ability in radians with dot product formula A dot B = |A||B|cos(theta)
+        float AbilityAngle = Mathf.Acos(dot / (Player.transform.up.magnitude * RelativeAttackEnd.magnitude)) * Mathf.Rad2Deg;
+
+        // Normalize the direction of the attack for incrementing the attack movement
+        Vector3 AttackNormal = (AttackEnd - Player.transform.position).normalized;
+        // Transform vector with quick if statements for returning offset for attacks
+        Vector3 AttackTransform = Player.transform.position + (RelativeAttackEnd.normalized * Ability.GetOffsetScale());
+        Transform AbilityTransform = Instantiate(Ability.gameObject, AttackTransform,
+            Quaternion.Euler(0, 0, AttackEnd.x >= Player.transform.position.x ? -AbilityAngle : AbilityAngle)).transform;
+        AbilityTransform.GetComponent<Ability>().SetUp(AttackNormal);
+        yield return new WaitForSeconds(Ability.GetCoolDownTime());
+
+        Player.toggleIsAttacking();
     }
 }
--- Dungeon Scramblers/Assets/Scripts/Hanson's/Scripts/HansonsOverlord.cs
+++ Dungeon Scramblers/Assets/Scripts/Hanson's/Scripts/HansonsOverlord.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class HansonsOverlord : Player
 {
+    enum AttackSequences
+    {
+        Basic
+    };
+
+
     [SerializeField]
     private LineRenderer line;
     [SerializeField]
     private Ability ability;
+    [SerializeField]
+    private AttackSequences sequence;
+
+    private Attack attack;
 
     bool bIsAttacking = false;
-    Vector3 AttackEnd;
-    Vector3 AttackDirection;
+
 
     protected override void OnEnable()
     {
@@ -27,6 +36,12 @@ public class HansonsOverlord : Player
         UpdateHandler.FixedUpdateOccurred -= ApplyMove;
     }
 
+    protected virtual void Awake()
+    {
+        base.Awake();
+        attack = this.gameObject.AddComponent<Attack>();
+    }
+
     // Override Attack function to start coroutine
     protected override void Attack(float f)
     { // MOUSE ATTACK INPUT
@@ -34,15 +49,15 @@ public class HansonsOverlord : Player
         if (usingOnScreenControls) return;
   
         Vector3 MouseWorldCoord = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        AttackDirection = new Vector3(MouseWorldCoord.x - transform.position.x , MouseWorldCoord.y - transform.position.y, 0);
-        if (!bIsAttacking) StartCoroutine("AttackSequence");
+        Vector3 AttackDirection = new Vector3(MouseWorldCoord.x - transform.position.x , MouseWorldCoord.y - transform.position.y, 0);
+        if (!bIsAttacking) attack.StartAttack(AttackDirection, ability, this);
     }
     protected override void Attack(Vector2 d)
     { // TOUCHSCREEN ATTACK INPUT
         if (!usingOnScreenControls) return;
         Debug.Log("Overlord attack on phone");
-        AttackDirection = new Vector3(-d.x, d.y, 0);
-        if (!bIsAttacking) StartCoroutine("AttackSequence");
+        Vector3 AttackDirection = new Vector3(-d.x, d.y, 0);
+        if (!bIsAttacking) attack.StartAttack(AttackDirection, ability, this);
     }
 
     public void toggleIsAttacking()
@@ -50,31 +65,31 @@ public class HansonsOverlord : Player
         bIsAttacking = !bIsAttacking;
     }
 
-    IEnumerator AttackSequence()
-    {
-        // Set Is currently attacking
-        bIsAttacking = true;
-        // Wait for ability casting time before proceeding
-        yield return new WaitForSeconds(ability.GetCastingTime());
-        // get mouse coordinate from camera when clicked and find the ending of the attack with the mouse clicked
+    //IEnumerator AttackSequence()
+    //{
+    //    // Set Is currently attacking
+    //    bIsAttacking = true;
+    //    // Wait for ability casting time before proceeding
+    //    yield return new WaitForSeconds(ability.GetCastingTime());
+    //    // get mouse coordinate from camera when clicked and find the ending of the attack with the mouse clicked
 
-        AttackEnd = transform.position + AttackDirection;
-        // Get relative position of where the mouse was clicked to correctly calculate the angle for projectile
-        Vector3 RelativeAttackEnd = AttackEnd - transform.position;
-        float dot = Vector3.Dot(transform.up, RelativeAttackEnd);
+    //    AttackEnd = transform.position + AttackDirection;
+    //    // Get relative position of where the mouse was clicked to correctly calculate the angle for projectile
+    //    Vector3 RelativeAttackEnd = AttackEnd - transform.position;
+    //    float dot = Vector3.Dot(transform.up, RelativeAttackEnd);
 
-        // Calculate the angle of the ability in radians with dot product formula A dot B = |A||B|cos(theta)
-        float AbilityAngle = Mathf.Acos(dot / (transform.up.magnitude * RelativeAttackEnd.magnitude)) * Mathf.Rad2Deg;
+    //    // Calculate the angle of the ability in radians with dot product formula A dot B = |A||B|cos(theta)
+    //    float AbilityAngle = Mathf.Acos(dot / (transform.up.magnitude * RelativeAttackEnd.magnitude)) * Mathf.Rad2Deg;
 
         
-        // Normalize the direction of the attack for incrementing the attack movement
-        Vector3 AttackNormal = (AttackEnd - transform.position).normalized;
-        // Transform vector with quick if statements for returning offset for attacks
-        Vector3 AttackTransform = transform.position + (RelativeAttackEnd.normalized * ability.GetOffsetScale());
-        Transform AbilityTransform = Instantiate(ability.gameObject, AttackTransform, 
-            Quaternion.Euler(0,0, AttackEnd.x >= transform.position.x ? -AbilityAngle : AbilityAngle)).transform;
-        AbilityTransform.GetComponent<Ability>().SetUp(AttackNormal);
-        yield return new WaitForSeconds(ability.GetCoolDownTime());
-        bIsAttacking = false;
-    }
+    //    // Normalize the direction of the attack for incrementing the attack movement
+    //    Vector3 AttackNormal = (AttackEnd - transform.position).normalized;
+    //    // Transform vector with quick if statements for returning offset for attacks
+    //    Vector3 AttackTransform = transform.position + (RelativeAttackEnd.normalized * ability.GetOffsetScale());
+    //    Transform AbilityTransform = Instantiate(ability.gameObject, AttackTransform, 
+    //        Quaternion.Euler(0,0, AttackEnd.x >= transform.position.x ? -AbilityAngle : AbilityAngle)).transform;
+    //    AbilityTransform.GetComponent<Ability>().SetUp(AttackNormal);
+    //    yield return new WaitForSeconds(ability.GetCoolDownTime());
+    //    bIsAttacking = false;
+    //}
 }
