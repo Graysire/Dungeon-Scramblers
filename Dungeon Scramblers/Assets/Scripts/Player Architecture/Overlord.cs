6f331dbe59ba2ad2d5dda731a15d4d21976923f4
nguyenhanson1 <nguyenhanson1@gmail.com>
2021-01-21 19:44:11 -0800

    fixed attack sequence function call within the overlord and implement player attack

--- Dungeon Scramblers/Assets/Scripts/Player Architecture/Overlord.cs
+++ Dungeon Scramblers/Assets/Scripts/Player Architecture/Overlord.cs
@@ -29,13 +29,13 @@ public class Overlord : Player
     protected override void Attack(float f)
     { // MOUSE ATTACK INPUT
         Vector3 MouseWorldCoord = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 AttackEnd = new Vector3(MouseWorldCoord.x, MouseWorldCoord.y, 0);
+        Vector3 AttackDirection = new Vector3(MouseWorldCoord.x - transform.position.x, MouseWorldCoord.y - transform.position.y, 0);
         // set the origin position of the attack indicator
-        line.SetPosition(0, transform.position);
+        //line.SetPosition(0, transform.position);
         // set the ending position of the attack indicator
-        line.SetPosition(1, AttackEnd);
-
-        Debug.DrawLine(transform.position, AttackEnd, Color.red, 10f);
+        //line.SetPosition(1, AttackEnd);
+        //Debug.DrawLine(transform.position, AttackEnd, Color.red, 10f);
+        if (allowedToAttack) AttackList[0].StartAttack(AttackDirection, this);
     }
     protected override void Attack(Vector2 d)
     { // TOUCHSCREEN ATTACK INPUT
