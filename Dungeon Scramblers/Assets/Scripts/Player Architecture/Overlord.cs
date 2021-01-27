bbe59710ecad0302d1aeff239f44786905b81cde
Jessica Wei <jessicawei9@gmail.com>
2021-01-26 18:53:46 -0800

    Overlord Q and E attacks are mapped

--- Dungeon Scramblers/Assets/Scripts/Player Architecture/Overlord.cs
+++ Dungeon Scramblers/Assets/Scripts/Player Architecture/Overlord.cs
@@ -10,12 +10,22 @@ public class Overlord : Player
         if (!usingOnScreenControls) {
             controls.PlayerMovement.UseAbilityQ.performed += ctx => UseAbilityQ(ctx.ReadValue<float>());
             controls.PlayerMovement.UseAbilityQ.canceled += ctx => UseAbilityQ(ctx.ReadValue<float>());
+            controls.PlayerMovement.UseAbilityE.performed += ctx => UseAbilityE(ctx.ReadValue<float>());
+            controls.PlayerMovement.UseAbilityE.canceled += ctx => UseAbilityE(ctx.ReadValue<float>());
         }
     }
 
-    protected void UseAbilityQ(float f) { 
+    protected void UseAbilityQ(float f) {
+        if (f < 1)
+            Debug.Log("Stop ability Q");
+        else if (f == 1)
+            ApplyAttack(f, 2);
     }
 
-    protected void UseAbilityE(float f) { 
+    protected void UseAbilityE(float f) {
+        if (f < 1)
+            Debug.Log("Stop ability E");
+        else if (f == 1)
+            ApplyAttack(f, 3);
     }
 }
