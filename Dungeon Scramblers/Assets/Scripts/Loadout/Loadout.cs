bacce3a6bb56e43792dbd072249d18406ea9a133
Kris <kristopher.kath@gmail.com>
2021-02-02 17:45:42 -0800

    Added experience dispersal on AI death, Added inventory class and equippable class

---
+++ Dungeon Scramblers/Assets/Scripts/Loadout/Loadout.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loadout : MonoBehaviour
+{
+    public List<Equippable> Equippables;    //Stores all equippables a player has
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Adds the given equippable into the loadout
+    public void AddEquippable(Equippable e)
+    {
+        Equippables.Add(e);
+    }
+}
