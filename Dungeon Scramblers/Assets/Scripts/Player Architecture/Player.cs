c007ab7793027f251a1fb2e80f89749e3e06e1a6
nguyenhanson1 <nguyenhanson1@gmail.com>
2021-02-07 20:56:55 -0800

    Change Stats and Affected Stats upon level up

--- Dungeon Scramblers/Assets/Scripts/Player Architecture/Player.cs
+++ Dungeon Scramblers/Assets/Scripts/Player Architecture/Player.cs
@@ -258,6 +258,28 @@ public class Player : HasStats, IDamageable<float>
         if(currentExperience >= expToNextLevel)
         {
             level++;
+            // Change the value of base stats
+            
+            // Change base stats of heatlh
+            stats[0] += stats[0] * 0.1f;
+            Debug.Log("Health: " + stats[0]);
+            // Change base stats of attack damage
+            stats[2] += stats[2] * 0.1f;
+            Debug.Log("Attack Damage: " + stats[2]);
+            // Change base stats of attack speed
+            stats[3] += stats[3] * 0.1f;
+            Debug.Log("Attack Speed: " + stats[3]);
+            // Change base stats of ability damage
+            stats[4] += stats[4] * 0.1f;
+            Debug.Log("Ability Damage: " + stats[4]);
+
+            // Refresh the affected stats
+            affectedStats[0] += stats[0] * 0.1f;
+            // refresh the affected stats upon level up
+            affectedStats[2] = stats[2];
+            affectedStats[3] = stats[3];
+            affectedStats[4] = stats[4];
+
             currentExperience = 0;
             
         }
