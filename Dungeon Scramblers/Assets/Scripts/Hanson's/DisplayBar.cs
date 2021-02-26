2feb8eca06f66609af7a779349d4b516bfcd84b0
nguyenhanson1 <nguyenhanson1@gmail.com>
2021-02-25 17:52:37 -0800

    create player UI for health and Experience to be update

---
+++ Dungeon Scramblers/Assets/Scripts/Hanson's/DisplayBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayBar : MonoBehaviour
+{
+
+    public Slider BarDisplay;
+    
+    public void SetValue(float ratio)
+    {
+        BarDisplay.value = ratio;
+    }
+
+}
