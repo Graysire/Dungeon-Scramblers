f81d5508441bc923cb244450c432d644a6a94b04
Graysire <graysire@dyrix.net>
2021-03-02 17:29:26 -0800

    Implemented equippable that adds status effectsand corresponding code to allow direct removal fo status effects

---
+++ Dungeon Scramblers/Assets/Scripts/Loadout/Equippables/StatusEffectEquippable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Equippable that applies a status effect to the equipper
+public class StatusEffectEquippable : Equippable
+{
+    [SerializeField]
+    StatusEffect[] effects;
+
+    public override void Equip(Player player)
+    {
+        base.Equip(player);
+        foreach (StatusEffect effect in effects)
+        {
+            player.AddStatusEffect(effect);
+        }
+    }
+
+    public override void Unequip(Player player)
+    {
+        base.Unequip(player);
+        foreach (StatusEffect effect in effects)
+        {
+            player.RemoveStatusEffect(effect);
+        }
+    }
+}
