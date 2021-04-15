bc6b54cfd172bf04646a933efb483f5a8909aa5d
nguyenhanson1 <nguyenhanson1@gmail.com>
2021-04-14 19:00:38 -0700

    Voting System implemented into objects, returning a result after the vote with the Button script to access the perk

---
+++ Dungeon Scramblers/Assets/Scripts/Hanson's/VoteButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VoteButton : MonoBehaviour
+{
+    [SerializeField]
+    private Perk perk;
+
+    public Perk getPerk() { return perk; }
+
+    TextMeshPro VoteCounter;
+    
+    int vote = 0;
+
+    public int getVote() { return vote; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject child = transform.GetChild(0).gameObject;
+        if (child)
+        {
+            VoteCounter = child.GetComponent<TextMeshPro>();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        VoteCounter.text = (++vote).ToString();
+        Debug.Log(vote);
+    }
+}
