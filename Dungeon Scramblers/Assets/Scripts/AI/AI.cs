8922aea9460ca32eb3ea67f427fe294b4b0475cb
Kristopher Kath <Kristopher.Kath@gmail.com>
2021-05-12 14:42:35 -0700

    Update AI.cs

--- Dungeon Scramblers/Assets/Scripts/AI/AI.cs
+++ Dungeon Scramblers/Assets/Scripts/AI/AI.cs
@@ -414,7 +414,7 @@ public class AI : AbstractPlayer
             gameObject.GetComponent<SpriteRenderer>().enabled = !enabled;
             this.canMove = false;
             allowedToAttack = false;
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(10.0f);
             Destroy(this.gameObject);
         }
     }
